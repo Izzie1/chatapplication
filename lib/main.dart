53f69f911a8e9246ceb70f465916d83cdbfbf5b5
= <=>
2021-02-28 12:29:14 +0700

    remake

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:funchat/screens/home.dart';
 import 'package:funchat/screens/login.dart';
-import 'package:funchat/services/firebase_repository.dart';
 
 void main() async {
 
