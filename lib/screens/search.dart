900c3f8a3f58b50b57f4413508c1588a82717af8
= <=>
2021-02-28 21:50:49 +0700

    implement search screen

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:funchat/models/account.dart';
+import 'package:funchat/services/firebase_repository.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -6,8 +8,46 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  FirebaseRepository _repository = new FirebaseRepository();
+  List<Account> accountList;
+  String query = "";
+  TextEditingController searchController = new TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _repository.getCurrentUser().then((user){
+      _repository.getAllAccount(user).then((listAccount){
+        setState(() {
+          accountList = listAccount;
+        });
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: 16,left: 16,right: 16),
+        child: TextField(
+          decoration: InputDecoration(
+            hintText: "Search...",
+            hintStyle: TextStyle(color: Colors.grey.shade400),
+            prefixIcon: Icon(Icons.search,color: Colors.grey.shade400,size: 20,),
+            filled: true,
+            fillColor: Colors.grey.shade100,
+            contentPadding: EdgeInsets.all(8),
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: BorderSide(
+                    color: Colors.grey.shade100
+                )
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
