8628bdfa3a82798a31f5914042d18652ad537f57
= <=>
2021-03-02 15:56:43 +0700

    change dummy data

--- lib/screens/pageviews/chat_list.dart
+++ lib/screens/pageviews/chat_list.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:funchat/components/chat_user_list.dart';
-import 'package:funchat/models/chatuser.dart';
+import 'package:funchat/screens/login.dart';
 import 'package:funchat/services/firebase_repository.dart';
 import 'package:funchat/ultilities/sizeconfig.dart';
+import 'package:funchat/ultilities/tile.dart';
+import 'package:funchat/ultilities/user_avatar.dart';
 import 'package:funchat/ultilities/widget.dart';
 
 import '../search.dart';
@@ -13,49 +14,6 @@ class ChatList extends StatefulWidget {
 }
 
 class _ChatListState extends State<ChatList> {
-  List<ChatUsers> chatUsers = [
-    ChatUsers(
-        text: "Jane Russel",
-        secondaryText: "Awesome Setup",
-        image: "images/userImage1.jpeg",
-        time: "Now"),
-    ChatUsers(
-        text: "Glady's Murphy",
-        secondaryText: "That's Great",
-        image: "images/userImage2.jpeg",
-        time: "Yesterday"),
-    ChatUsers(
-        text: "Jorge Henry",
-        secondaryText: "Hey where are you?",
-        image: "images/userImage3.jpeg",
-        time: "31 Mar"),
-    ChatUsers(
-        text: "Philip Fox",
-        secondaryText: "Busy! Call me in 20 mins",
-        image: "images/userImage4.jpeg",
-        time: "28 Mar"),
-    ChatUsers(
-        text: "Debra Hawkins",
-        secondaryText: "Thankyou, It's awesome",
-        image: "images/userImage5.jpeg",
-        time: "23 Mar"),
-    ChatUsers(
-        text: "Jacob Pena",
-        secondaryText: "will update you in evening",
-        image: "images/userImage6.jpeg",
-        time: "17 Mar"),
-    ChatUsers(
-        text: "Andrey Jones",
-        secondaryText: "Can you please share the file?",
-        image: "images/userImage7.jpeg",
-        time: "24 Feb"),
-    ChatUsers(
-        text: "John Wick",
-        secondaryText: "How are you?",
-        image: "images/userImage8.jpeg",
-        time: "18 Feb"),
-  ];
-
   static final FirebaseRepository _repository = FirebaseRepository();
   String currentUserId;
   String name;
@@ -85,28 +43,29 @@ class _ChatListState extends State<ChatList> {
           children: <Widget>[
             SafeArea(
                 child: Padding(
-              padding: EdgeInsets.only(left: 16, right: 16, top: 16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  UserAvatar(photo, name),
-                  Text(
-                    "Chat",
-                    style: customTextStyle(),
-                  ),
-                  Row(
-                    children: [
-                      IconButton(
-                        icon: Icon(
-                          Icons.edit,
-                          color: Colors.black,
-                        ),
+                  padding: EdgeInsets.only(left: 16, right: 16, top: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      UserAvatar(photo),
+                      Text(
+                        "Chat",
+                        style: customTextStyle(),
                       ),
-                    ],
-                  )
-                ],
-              ),
-            )),
+                      Row(
+                        children: [
+                          IconButton(
+                            icon: Icon(
+                            Icons.edit,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ),
             Padding(
               padding: EdgeInsets.all(16),
               child: InkWell(
@@ -134,20 +93,52 @@ class _ChatListState extends State<ChatList> {
               ),
             ),
             ListView.builder(
-              itemCount: chatUsers.length,
-              shrinkWrap: true,
-              padding: EdgeInsets.only(top: 0),
-              physics: NeverScrollableScrollPhysics(),
-              itemBuilder: (context, index) {
-                return ChatUsersList(
-                  text: chatUsers[index].text,
-                  secondaryText: chatUsers[index].secondaryText,
-                  image: chatUsers[index].image,
-                  time: chatUsers[index].time,
-                  isMessageRead: (index == 0 || index == 3) ? true : false,
-                );
-              },
-            ),
+                itemCount: 15,
+                shrinkWrap: true,
+                padding: EdgeInsets.all(12),
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return CustomTile(
+                    mini: false,
+                    onTap: () {},
+                    title: Text(
+                      "Dummy Data",
+                      style: TextStyle(
+                          color: Colors.orangeAccent, fontFamily: "Arial", fontSize: 19),
+                    ),
+                    subtitle: Text(
+                      "Hello",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 14,
+                      ),
+                    ),
+                    leading: Container(
+                        constraints: BoxConstraints(maxHeight: 50, maxWidth: 50),
+                        child: Stack(
+                          children: <Widget>[
+                            CircleAvatar(
+                              maxRadius: 50,
+                              backgroundColor: Colors.black,
+                              backgroundImage: AssetImage("images/avatar.png"),
+                            ),
+                            Align(
+                              alignment: Alignment.bottomRight,
+                              child: Container(
+                                height: 14,
+                                width: 14,
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.green,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                  );
+                }
+            )
           ],
         ),
       ),
@@ -155,50 +146,4 @@ class _ChatListState extends State<ChatList> {
   }
 }
 
-class UserAvatar extends StatelessWidget {
-  final String photoURL;
-  final String name;
 
-  UserAvatar(this.photoURL, this.name);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 40,
-      width: 40,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(50),
-        color: Colors.orangeAccent,
-      ),
-      child: Stack(
-        children: <Widget>[
-          Align(
-            alignment: Alignment.center,
-            child: photoURL != null
-                ? CircleAvatar(backgroundImage: NetworkImage(photoURL))
-                : Container(
-                    child: Text(
-                      name == null ? '' : name,
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14,
-                          color: Colors.white),
-                    ),
-                  ),
-          ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Container(
-              height: 12,
-              width: 12,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.green,
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
